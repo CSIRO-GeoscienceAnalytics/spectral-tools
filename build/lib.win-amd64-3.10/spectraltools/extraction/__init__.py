21f9b78cc72e43fcd76de2fdcb92666ce9ef8b6f
Andrew <32888047+AndyRodger@users.noreply.github.com>
2022-09-09 08:10:57 +0800

    Delete build directory

--- build/lib.win-amd64-3.10/spectraltools/extraction/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-A package for extracting spectral features from a spectral dataset. The returned features are ordered from the deepest
-to the smallest. The spectral data is accessed via an Instrument class in `spex.io.instruments`
-
-"""
-import sys
-sys.path.append("C:\2022\SpectralTools\spex\ext")
--- build/lib.win-amd64-3.10/spectraltools/io/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-A package for reading in spectral data from various sources.
-
-An abstract class (Instrument) is used to subclass 4 different representations of spectral data.
-
-These are CSV files, TSG datsets, image datasets and Numpy array datasets.
-
-"""
