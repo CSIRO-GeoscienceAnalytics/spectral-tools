437f07d01fb2de85d85fd4a1e1a8136c5f447f05
Sam Bradley <dam.bradley@csiro.au>
2023-02-20 16:12:47 +0800

    make cython truley optional

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import sys
 import textwrap
 
 import pkg_resources
-from Cython.Build import cythonize
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_py import build_py as _build_py
 
@@ -12,28 +11,37 @@ class build_py(_build_py):
         self.run_command("build_ext")
         return super().run()
 
+
 try:
-    pkg_resources.require('setuptools >= 18.0')
+    pkg_resources.require("setuptools >= 18.0")
 except pkg_resources.ResolutionError:
-    print(textwrap.dedent("""
+    print(
+        textwrap.dedent(
+            """
         setuptools >= 18.0 is required, and the dependency cannot be
         automatically resolved with the version of setuptools that is
         currently installed (%s).
 
         You can upgrade setuptools:
         $ pip install -U setuptools
-        """ % pkg_resources.get_distribution("setuptools").version),
-          file=sys.stderr)
+        """
+            % pkg_resources.get_distribution("setuptools").version
+        ),
+        file=sys.stderr,
+    )
     sys.exit(1)
 
 # Here we try to import Cython - if it's here then we can generate new c sources
 # directly from the pyx files using their build_ext class.
 # If not then we just use the default setuptools version
 try:
+    from Cython.Build import cythonize
     from Cython.Distutils import build_ext
+
     HAVE_CYTHON = True
 except ImportError:
     from setuptools.command.build_ext import build_ext
+
     # from distutils.command.build_ext import build_ext
     HAVE_CYTHON = False
 
@@ -45,30 +53,30 @@ HAVE_CYTHON = False
 #     'build_ext': build_ext
 # }
 extensions = {}
-ext = '.pyx' if HAVE_CYTHON else '.c'
+ext = ".pyx" if HAVE_CYTHON else ".c"
 
-extensions = [Extension("spectraltools.ext.chulls", ["spectraltools/ext/chulls"+ext])]
+extensions = [Extension("spectraltools.ext.chulls", ["spectraltools/ext/chulls" + ext])]
 
 if HAVE_CYTHON:
-    #extensions = [Extension("spex.ext.chulls", ["spex/ext/chulls.pyx"])]
+    # extensions = [Extension("spex.ext.chulls", ["spex/ext/chulls.pyx"])]
     compiler_directives = {"language_level": 3, "embedsignature": True}
     extensions = cythonize(extensions, compiler_directives=compiler_directives)
 
 setup(
-    name='spectraltools',
-    version='0.1.67',
+    name="spectraltools",
+    version="0.1.67",
     packages=find_packages(),
-    package_data={'spectraltools': ['ext/*.pyd', 'ext/*.so']},
+    package_data={"spectraltools": ["ext/*.pyd", "ext/*.so"]},
     include_package_data=True,
-    url='',
-    author='Andrew Rodger',
-    license='MIT',
-    description='A collection of tools that I have used for working with hyperspectral data',
-    install_requires=['numpy', 'spectral', 'pandas', 'scipy', 'scikit-learn'],
-    python_requires='>=3.8',
-    setup_requires=['numpy', 'wheel'],
+    url="",
+    author="Andrew Rodger",
+    license="MIT",
+    description="A collection of tools that I have used for working with hyperspectral data",
+    install_requires=["numpy", "spectral", "pandas", "scipy", "scikit-learn"],
+    python_requires=">=3.8",
+    setup_requires=["numpy", "wheel"],
     zip_safe=False,
     # Cython extensions & other stuff
-    cmdclass={'build_py': build_py, 'build_ext': build_ext},
-    ext_modules=extensions
+    cmdclass={"build_py": build_py, "build_ext": build_ext},
+    ext_modules=extensions,
 )
